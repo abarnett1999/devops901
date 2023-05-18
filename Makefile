0b8e791dac9c371b23d6b38bda56bca1a7c2fb47
Amani Barnett <abarnett1999@yahoo.com>
2023-05-18 12:55:28 +0000

    adding inital structure

---
+++ Makefile
@@ -0,0 +1,16 @@
+
+
+
+install:
+	pip install --upgrade pip &&\
+		pip install -r requirements.txt
+lint:
+	pylint --disable=R,C hello.py
+
+format:
+	black *.py
+
+test:
+	python -m pytest -vv --cov=hello test_hello.py	
+
+all:install lint test
---
+++ hello.py
@@ -0,0 +1,17 @@
+# def add(x,y):
+#     return x + y
+
+# x=1
+# y=2
+# result = add(x,y)
+# print(f"This is the sum: {x}, {y}, {result}")
+
+
+# cleaned up code after running lint
+def add(x, y):
+    return x + y
+
+
+# =kk
+result = add(1, 2)
+print(f"This is the sum: 1, 2, {result}")
